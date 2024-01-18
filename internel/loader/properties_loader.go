017dd080d5ef238a8c8b78ef6d488aa76e67539b
photowey <photowey@gmail.com>
2024-01-18 21:07:35 +0800

    feat: Add: 1.loader package PropertiesConfigLoader
    Signed-off-by: photowey <photowey@gmail.com>

---
+++ internel/loader/properties_loader.go
@@ -0,0 +1,57 @@
+/*
+ * Copyright © 2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package loader
+
+import (
+	"github.com/photowey/nemo/pkg/collection"
+	"github.com/photowey/nemo/pkg/ordered"
+	"github.com/photowey/nemo/pkg/stringz"
+)
+
+// xxx.properties
+
+const (
+	Properties = "properties"
+)
+
+const (
+	propertiesStep     = 1000
+	propertiesPriority = ordered.HighPriority + propertiesStep*ordered.DefaultStep
+)
+
+var (
+	propertiesSupportedConfigTypes = stringz.InitStringSlice(Properties)
+)
+
+var (
+	_ ConfigLoader = (*PropertiesConfigLoader)(nil)
+)
+
+type PropertiesConfigLoader struct {
+}
+
+func (pcl *PropertiesConfigLoader) Supports(strategy string) bool {
+	return collection.ArrayContains(propertiesSupportedConfigTypes, strategy)
+}
+
+func (pcl *PropertiesConfigLoader) Order() int64 {
+	return propertiesPriority
+}
+
+func (pcl *PropertiesConfigLoader) Load(path string, targetPtr any) {
+
+}
